dfbe693c614e80e49805a8fc4bf1256801aa3844
SulemanRasheed132446 <53094032+SulemanRasheed132446@users.noreply.github.com>
2021-01-09 01:32:37 +0500

    Updated Upload method for stream uploading of file

--- utils/Bucket.js
+++ utils/Bucket.js
@@ -1,9 +1,9 @@
 const {bucket} = require('./firebase');
 const {uuid} = require('uuidv4')
 
-function generateMetadataObject(fileName) {
+function generateMetadataObject({fileName, mimeType}) {
     const token = uuid();
-    const metaData = {
+    const metadata = {
       contentType: `image/jpeg`,  // contentType IS OPTIONAL CAUSE IT WILL BE AUTO-INFERRED
       cacheControl: `public,max-age=31536000,must-revalidate`,  // YOU CAN ALSO USE IT TO ADD OTHER METADATA LIKE cacheControl
       contentDisposition: `inline; filename*=utf-8''${fileName}`,
@@ -12,20 +12,33 @@ function generateMetadataObject(fileName) {
       }
     };
     return {
-        metaData,
+        metadata,
         token
     }
 }
 
-async function uploadImage (path, destinationPath) {
-    const {metaData, token} = await generateMetadataObject(path);
-    await bucket.upload(path, {
-        metadata: metaData,
-        destination: destinationPath
+async function uploadImage (file, destinationPath) {
+    const {stream, filename, mimetype, encoding} =  await file
+    const fireStorageFile = bucket.file(`${destinationPath}`)
+    const {metadata, token} = generateMetadataObject({fileName: filename, mimetype})
+    const writeStream = fireStorageFile.createWriteStream({
+       metadata,
     })
-    return `https://firebasestorage.googleapis.com/v0/b/${'restaurant-dev-3c85f.appspot.com'}/o/${path}?alt=media&token=${token}`
+
+    const result =  new Promise((resolve, reject) =>{
+       writeStream.on('error', function (err) {
+          reject(err);
+       });
+       writeStream.on('finish', function () {	
+          resolve(`https://firebasestorage.googleapis.com/v0/b/${'restaurant-dev-3c85f.appspot.com'}/o/${destinationPath}?alt=media&token=${token}`);
+        });
+    })
+
+    stream.pipe(writeStream)
+    return result
 }
 
 module.exports = {
-    uploadImage
+    uploadImage,
+    generateMetadataObject
 }
